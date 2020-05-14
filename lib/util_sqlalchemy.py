dbf89778ea79f5cbaacfb27ca777c3dab913864e
rijan <rijanadhikari@gmail.com>
2020-05-14 09:53:32 +0545

    added utils for sort and sorting

--- lib/util_sqlalchemy.py
+++ lib/util_sqlalchemy.py
@@ -33,6 +33,72 @@ class ResourceMixin(object):
                            default=tzware_datetime,
                            onupdate=tzware_datetime)
 
+    @classmethod
+    def sort_by(cls, field, direction):
+        """
+        Validate the sort field and direction.
+
+        :param field: Field name
+        :type field: str
+        :param direction: Direction
+        :type direction: str
+        :return: tuple
+        """
+        if field not in cls.__table__.columns:
+            field = 'created_on'
+
+        if direction not in ('asc', 'desc'):
+            direction = 'asc'
+
+        return field, direction
+
+    @classmethod
+    def get_bulk_action_ids(cls, scope, ids, omit_ids=[], query=''):
+        """
+        Determine which IDs are to be modified.
+
+        :param scope: Affect all or only a subset of items
+        :type scope: str
+        :param ids: List of ids to be modified
+        :type ids: list
+        :param omit_ids: Remove 1 or more IDs from the list
+        :type omit_ids: list
+        :param query: Search query (if applicable)
+        :type query: str
+        :return: list
+        """
+        omit_ids = map(str, omit_ids)
+
+        if scope == 'all_search_results':
+            # Change the scope to go from selected ids to all search results.
+            ids = cls.query.with_entities(cls.id).filter(cls.search(query))
+
+            # SQLAlchemy returns back a list of tuples, we want a list of strs.
+            ids = [str(item[0]) for item in ids]
+
+        # Remove 1 or more items from the list, this could be useful in spots
+        # where you may want to protect the current user from deleting themself
+        # when bulk deleting user accounts.
+        if omit_ids:
+            ids = [id for id in ids if id not in omit_ids]
+
+        return ids
+
+    @classmethod
+    def bulk_delete(cls, ids):
+        """
+        Delete 1 or more model instances.
+
+        :param ids: List of ids to be deleted
+        :type ids: list
+        :return: Number of deleted instances
+        """
+        delete_count = cls.query.filter(cls.id.in_(ids)).delete(
+            synchronize_session=False)
+        db.session.commit()
+
+        return delete_count
+
     def save(self):
         """
         Save a model instance.
