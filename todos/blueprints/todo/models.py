d6a588b210eb71ba7060f14e659f1475228de8f6
rijan <rijanadhikari@gmail.com>
2020-05-20 07:28:10 +0545

    updated todo model

--- todos/blueprints/todo/models.py
+++ todos/blueprints/todo/models.py
@@ -1,15 +1,18 @@
-from lib.util_sqlalchemy import ResourceMixin
+from lib.util_sqlalchemy import ResourceMixin, AwareDateTime
 from todos.extensions import db
 
 
 class Todo(ResourceMixin, db.Model):
     __tablename__ = 'todo'
 
-    id = db.Column(db.Integer, primary_key=True)
-
-    active = db.Column('is_active', db.Boolean(), nullable=False,
-                       server_default='1')
-    username = db.Column(db.String(24), unique=True, index=True)
-    email = db.Column(db.String(255), unique=True, index=True, nullable=False,
-                      server_default='')
-    password = db.Column(db.String(128), nullable=False, server_default='')
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('user_id',
+              db.Integer,
+              db.ForeignKey('users.id',
+                            onupdate='CASCADE',
+                            ondelete='CASCADE'),
+              index=True,
+              nullable=False),
+    db.Column('description', db.Text, nullable=False),
+    db.Column('todo_at', AwareDateTime(), nullable=False),
+    db.Column('is_complete', db.Boolean(), nullable=False, server_default='1'),
