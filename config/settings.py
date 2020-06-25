9d7c5d096e3f7ecc8a8a92aef69d22a8e2d10c44
rijan <rijanadhikari@gmail.com>
2020-06-25 09:05:10 +0545

    updated config

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,12 @@ DEBUG = bool(os.getenv('DEBUG', False))
 SECRET_KEY = os.getenv('SECRET_KEY', 'secrect_key')
 SERVER_NAME = os.getenv('SERVER_NAME', 'localhost:8000')
 
+# AWS
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_BUCKET = os.getenv('AWS_BUCKET')
+AWS_REGION = os.getenv('AWS_REGION')
+
 # SQLAlchemy.
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', '')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
